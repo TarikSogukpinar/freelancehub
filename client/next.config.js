d2a10a5148c15a24e46a2e5cde900973e9e9ffa4
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-04-30 15:33:42 +0300

    services files are created and next.config.js modified & added env variables

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SERVER_HOST: process.env.SERVER_HOST,
+  },
 }
 
 module.exports = nextConfig
---
+++ client/src/services/authService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export const registerUser = async (
+  firstName,
+  lastName,
+  email,
+  password,
+  confirmPassword
+) => {
+  return await axios.post(
+    `${process.env.SERVER_HOST}/api/auth/register`,
+    {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    },
+    { withCredentials: true }
+  );
+};
+
+export const loginUser = async (email, password) => {
+  return await axios.post(
+    `${process.env.SERVER_HOST}/api/auth/login`,
+    {
+      email,
+      password,
+    },
+    { withCredentials: true }
+  );
+};
+
+export const logoutUser = async () => {
+  return await axios.get(`${process.env.SERVER_HOST}/api/auth/logout`, {
+    credentials: "include",
+    withCredentials: true,
+  });
+};
