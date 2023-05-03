71e33995e932e874e51bd62e3656ad2f5217f527
Halit Uzan <halit.uzan@gmail.com>
2023-05-03 12:40:51 +0300

    delete old package

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 import HomePage from "../../components/HomePage";
-
-
 
 export default function Home() {
   return (
