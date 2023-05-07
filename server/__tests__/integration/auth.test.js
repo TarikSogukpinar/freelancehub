82ec41e3957c8bfe64719099fbcba32d1856b386
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-05-07 15:29:27 +0300

    test files created

--- server/__tests__/integration/auth.test.js
+++ server/__tests__/integration/auth.test.js
@@ -1,1 +1,18 @@
-const request = require('supertest');
+import supertest from "supertest";
+import app from "../../integration/app.js";
+
+const base_url = "http://localhost:5000/api/auth";
+
+describe("POST /users", () => {
+  describe("given a username and password", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await supertest(app).post(`${base_url}/login`).send({
+        username: "testuser",
+        password: "testpassword",
+      });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe("when the username and password is missing", () => {});
+});
