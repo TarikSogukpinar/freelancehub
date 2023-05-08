e29c48c65d4292f2371f78b7809fe87aa4ac7106
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-05-08 22:41:55 +0300

    cors option env changes added

--- server/helpers/cors/corsOption.js
+++ server/helpers/cors/corsOption.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
 dotenv.config({
-  path: "./.env.local",
+  path: envFile,
 });
 
 const corsOption = {
